7bdf2dd4ee6937db464a3107c7fec2cf15fde9c7
Lucas Vieira <85909450+LucasVieira757@users.noreply.github.com>
2026-03-26 12:05:09 -0300

    Add files via upload

---
+++ web/src/app/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{color: "white", background:"#0F172A", height:"100vh", padding:"20px"}}>
+      <h1>Template Dashboard</h1>
+      <p>Use como base visual para Power BI</p>
+    </div>
+  )
+}
